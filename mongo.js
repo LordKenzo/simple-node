82c0a8a2ca72f46e5376da89b3db2743b2a93616
LordKenzo <lordkenzo@outlook.it>
2019-04-18 18:03:41 +0200

    arifix

--- mongo.js
+++ mongo.js
@@ -3,12 +3,13 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 const dbName = 'meetupsDB';
-const mongoUri = process.env.CONNECTION_STRING || `mongodb://127.0.0.1:27017/${dbName}`;
+const mongoUri =
+  `mongodb://lordkenzo-azure:SYVXdXvnrooOU0mDl3apBLx0LmV6Qs1egQm0W6wVoV3AgdGHnanCSnYZciEkKdEyyS5vCHDSXEIk4lk7138ghw==@lordkenzo-azure.documents.azure.com:10255/${dbName}?ssl=true` ||
+  `mongodb://127.0.0.1:27017/${dbName}`;
 
 function connect() {
   mongoose.set('debug', true);
   return mongoose.connect(mongoUri, {
-    useMongoClient: true,
     useCreateIndex: true,
     useNewUrlParser: true,
   });
