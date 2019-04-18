ee05099b85c04f32b2cc5fe703847b79f9b357c4
LordKenzo <lordkenzo@outlook.it>
2019-04-18 17:30:37 +0200

    test

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
 const express = require('express');
-const path = require('path');
-const routes = require('./routes');
-
+const app = express();
 const port = process.env.PORT || 3000;
 
-const app = express();
-
-app.use('/api', routes);
-
 app.get('/', (req, res) => {
-  res.send('Hello World from Azure and Git on Github 🦄!');
+  res.send('Hello World from Azure and Git on Github🦄!');
 });
 
 app.listen(port, () => console.log(`listening on http://localhost:${port}`));
