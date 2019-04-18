f180ae5146917d053f594299da43b0311823378f
LordKenzo <lordkenzo@outlook.it>
2019-04-18 13:19:49 +0200

    msg from github 💥

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World from Azure and Azure Git🦄!');
+  res.send('Hello World from Azure and Git on Github 🦄!');
 });
 
 app.listen(port, () => console.log(`listening on http://localhost:${port}`));
