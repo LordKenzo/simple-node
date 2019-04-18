79f7a35d7958cbc6927ab9175efbe6260d89e9c3
LordKenzo <lordkenzo@outlook.it>
2019-04-18 18:02:11 +0200

    fix mongouri

--- mongo.js
+++ mongo.js
@@ -3,12 +3,11 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 const dbName = 'meetupsDB';
-const mongoUri = `mongodb://lordkenzo-azure:SYVXdXvnrooOU0mDl3apBLx0LmV6Qs1egQm0W6wVoV3AgdGHnanCSnYZciEkKdEyyS5vCHDSXEIk4lk7138ghw==@lordkenzo-azure.documents.azure.com:10255/${dbName}?ssl=true`;
-//process.env.CONNECTION_STRING || 'mongodb://127.0.0.1:27017';
+const mongoUri = process.env.CONNECTION_STRING || `mongodb://127.0.0.1:27017/${dbName}`;
 
 function connect() {
   mongoose.set('debug', true);
-  return mongoose.connect(`${mongoUri}/${dbName}`, {
+  return mongoose.connect(mongoUri, {
     useMongoClient: true,
     useCreateIndex: true,
     useNewUrlParser: true,
