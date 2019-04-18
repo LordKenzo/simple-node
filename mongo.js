a0da8e2dfab19d6cc1749bd697f235c84fedffb5
LordKenzo <lordkenzo@outlook.it>
2019-04-18 17:56:05 +0200

    modifice uri

--- mongo.js
+++ mongo.js
@@ -2,13 +2,14 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-const mongoUri = process.env.CONNECTION_STRING || 'mongodb://127.0.0.1:27017';
-
 const dbName = 'meetupsDB';
+const mongoUri = `mongodb://lordkenzo-azure:SYVXdXvnrooOU0mDl3apBLx0LmV6Qs1egQm0W6wVoV3AgdGHnanCSnYZciEkKdEyyS5vCHDSXEIk4lk7138ghw==@lordkenzo-azure.documents.azure.com:10255/${dbName}?ssl=true`;
+//process.env.CONNECTION_STRING || 'mongodb://127.0.0.1:27017';
 
 function connect() {
   mongoose.set('debug', true);
   return mongoose.connect(`${mongoUri}/${dbName}`, {
+    useMongoClient: true,
     useCreateIndex: true,
     useNewUrlParser: true,
   });
