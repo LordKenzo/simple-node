af441eec32ae6e1d4c1e240568668cbc5141ef49
LordKenzo <lordkenzo@outlook.it>
2019-04-18 17:19:02 +0200

    connessione

--- mongo.js
+++ mongo.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-const mongoUri = process.env.CONNECTION_STRING || 'mongodb://127.0.0.1:27017/meetupsDB';
+const mongoUri = process.env.CONNECTION_STRING;
 
 function connect() {
   mongoose.set('debug', true);
